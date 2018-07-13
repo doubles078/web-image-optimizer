6a3ad5f91ff247af7c052d1fa3044c629d0f33e9
Dan Donohue <dan.donohue@ef.com>
2018-07-13 17:10:33 -0400

    Updating to walk through all dirs

--- imageopt.py
+++ imageopt.py
@@ -1,52 +1,33 @@
-#run this in any directory add -v for verbose 
+#run this in any directory add -v for verbose
 #pip install Image
 import os
 import sys
 from PIL import Image
 
-def buildDirectories(file):
-	pwd = os.getcwd()
+def buildDirectories(file, subdir):
+	pwd = subdir
 	#makes two new dirs for compressed and resized
-	new_directories = [pwd + '/compressed', pwd + '/compressed_and_resized']
+	new_directory = pwd + '/compressed'
+	new_filename = 'Compressed_' + pwd.replace('/', '_') + file
 
-	for directory in new_directories:
-		if not os.path.exists(directory):
-			os.mkdir(directory)
-		
-	path_to_compressed = new_directories[0] + '/Compressed_' + file
-	path_to_resized = new_directories[1] + '/Smaller_' + file
+	if not os.path.exists(new_directory):
+		os.mkdir(new_directory)
 
-	return [path_to_compressed, path_to_resized]
+	path_to_compressed = new_directory + '/Compressed_' + file
 
+	return path_to_compressed
 
-def resizeImages(picture, path_to_resized, compress_quality):
-	#Static width in pixels.  Could also change this to be a param in the CLI
-	new_width = 300
-	width, height = picture.size
-	new_height = int(new_width * height / width)
-	picture_resized = picture.resize((new_width, new_height), Image.ANTIALIAS)
-
-	#Save the compressed and resized image to the correct path
-	picture_resized.save(path_to_resized, optimize=True,quality=compress_quality)
-
-	return picture_resized
-
-
-def compressImages(file,  verbose=False):
+def compressImages(file, filepath, subdir, verbose=False):
 	#What percentage quality of the original image to keep. 65 to 85 is best bet here
-	compress_quality = 85
-
-	filepath = os.path.join(os.getcwd(), file)
+	compress_quality = 80
 	oldsize = os.stat(filepath).st_size
 	picture = Image.open(filepath)
 
 	#Build the directories
-	path_compress = buildDirectories(file)[0]
-	path_resize = buildDirectories(file)[1]
+	path_compress = buildDirectories(file, subdir)
 
 	#Save the compressed pic to its correct path
-	picture.save(path_compress,optimize=True,quality=compress_quality) 
-	resizeImages(picture, path_resize, compress_quality)
+	picture.save(path_compress,optimize=True,quality=compress_quality)
 
 	#Log information about the compression savings
 	newsize = os.stat(os.path.join(os.getcwd(), path_compress)).st_size
@@ -69,10 +50,12 @@ def main():
 
 	tot = 0
 	num = 0
-	for file in os.listdir(pwd):
-		if os.path.splitext(file)[1].lower() in ('.jpg', '.jpeg', '.png'):
-			num += 1
-			tot += compressImages(file, verbose)
+	for subdir, dirs, files in os.walk(pwd):
+		for file in files:
+			if os.path.splitext(file)[1].lower() in ('.jpg', '.jpeg', '.png'):
+				filepath = os.path.join(subdir, file)
+				num += 1
+				tot += compressImages(file, filepath, subdir, verbose)
 	print("Average Compression: %d" % (float(tot)/num))
 	print("Done")
 
